37b86f871274a58c115a31c9d42a83b8ef8f078a
Cleciana M. Santana <cleciana.santana@ccc.ufcg.edu.br>
2019-09-23 17:28:08 -0300

    rot6

---
+++ R06-02-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -0,0 +1,33 @@
+package adt.linkedList;
+
+public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
+		DoubleLinkedList<T> {
+
+	protected DoubleLinkedListNode<T> last;
+
+	@Override
+	public void insertFirst(T element) {
+		
+	}
+
+	@Override
+	public void removeFirst() {
+		SingleLinkedListNode<T> node = this.getHead();
+		this.setHead(node.getNext());
+	}
+
+	@Override
+	public void removeLast() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	public DoubleLinkedListNode<T> getLast() {
+		return last;
+	}
+
+	public void setLast(DoubleLinkedListNode<T> last) {
+		this.last = last;
+	}
+
+}
---
+++ R06-02-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/linkedList/SingleLinkedListImpl.java
@@ -0,0 +1,93 @@
+package adt.linkedList;
+
+public class SingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected SingleLinkedListNode<T> head;
+
+	public SingleLinkedListImpl() {
+		this.head = new SingleLinkedListNode<T>();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.head.isNIL();
+	}
+
+	@Override
+	public int size() {
+		SingleLinkedListNode<T> node = this.getHead();
+		int s = 0;
+
+		while (!node.isNIL()) {
+			s += 1;
+			node = node.getNext();
+		}
+		return s;
+	}
+
+	@Override
+	public T search(T element) {
+		SingleLinkedListNode<T> node = this.getHead();
+
+		while (!node.isNIL()) {
+			if (node.getData().equals(element)) {
+				return node.getData();
+			}
+			node = node.getNext();
+		}
+		return null;
+	}
+
+	@Override
+	public void insert(T element) {
+		if (element != null) {
+
+			SingleLinkedListNode<T> node = this.getHead();
+			while (!node.isNIL()) {
+				node = node.getNext();
+			}
+			node.setData(element);
+			node.setNext(new SingleLinkedListNode<>());
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		SingleLinkedListNode<T> node = this.getHead();
+
+		while (!node.isNIL()) {
+			
+			if (node.getNext().getData().equals(element)) {
+				node.setNext(node.getNext().getNext());
+				return;
+			} 
+			node = node.getNext();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T[] toArray() {
+		
+		SingleLinkedListNode<T> node = this.getHead();
+		int size = this.size();
+		T[] array = (T[]) new Comparable[size];
+		int i = 0;
+		
+		while(!node.isNIL() && i < size) {
+			array[i] = node.getData();
+			node = node.getNext();
+			i += 1;
+		}
+		return array;
+	}
+
+	public SingleLinkedListNode<T> getHead() {
+		return head;
+	}
+
+	public void setHead(SingleLinkedListNode<T> head) {
+		this.head = head;
+	}
+
+}
