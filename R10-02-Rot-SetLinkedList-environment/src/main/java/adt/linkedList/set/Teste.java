43399f76e7ff1e7452511dd5aab800078cf7e109
Cleciana M. Santana <cleciana.santana@ccc.ufcg.edu.br>
2019-10-10 15:44:58 -0300

    alguns testes

--- R10-02-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/Teste.java
+++ R10-02-Rot-SetLinkedList-environment/src/main/java/adt/linkedList/set/Teste.java
@@ -9,10 +9,12 @@ import org.junit.Test;
 public class Teste {
 
 	private SetLinkedListImpl<Integer> set;
+	private SetLinkedListImpl<Integer> aux;
 	
 	@Before
 	public void setUp() {
 		set = new SetLinkedListImpl<>();
+		aux = new SetLinkedListImpl<>();
 		
 		set.insert(1);
 		set.insert(2);
@@ -24,6 +26,14 @@ public class Teste {
 		set.insert(5);
 		set.insert(7);
 		
+		aux.insert(5);
+		aux.insert(10);
+		aux.insert(25);
+		aux.insert(13);
+		aux.insert(4);
+		aux.insert(9);
+		aux.insert(10);
+		aux.insert(1);
 	}
 	@Test
 	public void testRemoveDuplicates() {
@@ -34,17 +44,23 @@ public class Teste {
 
 	@Test
 	public void testUnion() {
-		fail("Not yet implemented");
+		SetLinkedListImpl<Integer> aux = new SetLinkedListImpl<>();
+		SetLinkedList<Integer> test = set.union(aux);
+
+		Assert.assertArrayEquals(new Integer[] {1, 2, 3, 5, 7, 10, 25, 13, 4, 9}, test.toArray());
 	}
 
 	@Test
 	public void testIntersection() {
-		fail("Not yet implemented");
+		SetLinkedList<Integer> test = set.intersection(aux);
+		
+		Assert.assertArrayEquals(new Integer[] {1, 5}, test.toArray());
 	}
 
 	@Test
 	public void testConcatenate() {
-		fail("Not yet implemented");
+		set.concatenate(aux);
+		Assert.assertArrayEquals(set.toArray(), new Integer[] {1, 2, 3, 5, 7, 10, 25, 13, 4, 9});
 	}
 
 }
