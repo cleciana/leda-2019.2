fbbcd799f55bb51b35f2f5c75b3e30154b37958f
Cleciana M. Santana <cleciana.santana@ccc.ufcg.edu.br>
2019-10-21 17:18:03 -0300

    rot10

---
+++ R11-02-Rot-Arvore-Binaria-de-Busca-environment/src/main/java/adt/bst/BSTImpl.java
@@ -0,0 +1,183 @@
+package adt.bst;
+
+import adt.bt.BTNode;
+
+public class BSTImpl<T extends Comparable<T>> implements BST<T> {
+
+	protected BSTNode<T> root;
+
+	public BSTImpl() {
+		root = new BSTNode<T>();
+	}
+
+	public BSTNode<T> getRoot() {
+		return this.root;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return root.isEmpty();
+	}
+
+	@Override
+	public int height() {
+		return this.height(root);
+	}
+
+	private int height(BSTNode<T> node) {
+
+		if (node.isEmpty())
+			return -1;
+
+		return 1 + Math.max(height((BSTNode<T>) node.getLeft()), height((BSTNode<T>) node.getRight()));
+	}
+
+	@Override
+	public BSTNode<T> search(T element) {
+		if (element != null) {
+			return this.search(root, element);
+		}
+		return new BSTNode<>();
+	}
+
+	private BSTNode<T> search(BSTNode<T> node, T element) {
+		if (!node.isEmpty()) {
+
+			if (node.getData().compareTo(element) == 0) {
+				return node;
+
+			} else if (node.getData().compareTo(element) < 0) {
+				return search((BSTNode<T>) node.getRight(), element);
+
+			} else {
+				search((BSTNode<T>) node.getLeft(), element);
+			}
+		}
+		return new BSTNode<>();
+	}
+
+	@Override
+	public void insert(T element) {
+		if (element != null)
+			this.insert(root, element);
+	}
+
+	private void insert(BSTNode<T> node, T element) {
+
+		if (node.isEmpty()) {
+			node.setData(element);
+
+		} else if (node.getData().compareTo(element) < 0) {
+			if (node.getRight() == null) {
+				node.setRight(new BSTNode<T>());
+				node.getRight().setData(element);
+
+			} else {
+				this.insert((BSTNode<T>) node.getRight(), element);
+			}
+		} else if (node.getData().compareTo(element) > 0) {
+			if (node.getLeft() == null) {
+				node.setLeft(new BSTNode<T>());
+				node.getLeft().setData(element);
+
+			} else {
+				this.insert((BSTNode<T>) node.getLeft(), element);
+			}
+		}
+	}
+
+	@Override
+	public BSTNode<T> maximum() {
+		if (this.isEmpty())
+			return null;
+
+		return maximum(root.getRight());
+	}
+
+	private BSTNode<T> maximum(BTNode<T> node) {
+
+		if (node.getRight().isEmpty())
+			return (BSTNode<T>) node;
+
+		return maximum(node.getRight());
+	}
+
+	@Override
+	public BSTNode<T> minimum() {
+		if (this.isEmpty())
+			return null;
+
+		return minimum(root.getLeft());
+	}
+
+	private BSTNode<T> minimum(BTNode<T> node) {
+
+		if (node.getLeft().isEmpty())
+			return (BSTNode<T>) node;
+
+		return minimum(node.getLeft());
+	}
+
+	@Override
+	public BSTNode<T> sucessor(T element) {
+		BSTNode<T> aux = this.search(element);
+
+		if (!aux.isEmpty()) {
+			return minimum(aux.getRight());
+		}
+		return null;
+	}
+
+	@Override
+	public BSTNode<T> predecessor(T element) {
+		BSTNode<T> aux = this.search(element);
+
+		if (!aux.isEmpty()) {
+			return maximum(aux.getLeft());
+		}
+		return null;
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] preOrder() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] order() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] postOrder() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	/**
+	 * This method is already implemented using recursion. You must understand how
+	 * it work and use similar idea with the other methods.
+	 */
+	@Override
+	public int size() {
+		return size(root);
+	}
+
+	private int size(BSTNode<T> node) {
+		int result = 0;
+		// base case means doing nothing (return 0)
+		if (!node.isEmpty()) { // indusctive case
+			result = 1 + size((BSTNode<T>) node.getLeft()) + size((BSTNode<T>) node.getRight());
+		}
+		return result;
+	}
+
+}
